e4dfdf31c4f7a3af9e090cf5cfd7ad44eb6c0e6a
kylea <kyleanderson0112@gmail.com>
2024-04-05 21:06:34 -0400

    Scrapping genre data, script completed

--- getGenre.py
+++ getGenre.py
@@ -19,15 +19,29 @@ headers = {
 
 rows = cursor.fetchall()
 
+cursor.execute("SELECT game_id FROM genres;")
+completed_rows = [r[0] for r in cursor.fetchall()]
+
 
 pattern = re.compile(r'genre', re.IGNORECASE)
 
+row_length = len(rows)
+count = 0
+
 for row in rows:
+    count += 1
+    print((count)/row_length)
     print(row[0])
-    page = requests.get("https://howlongtobeat.com/game/" + str(row[0]), headers=headers)
-    if page.status_code == 200:
-        soup = bs(page.content, "html.parser")
-        div = soup.find_all("div", class_='GameSummary_profile_info__HZFQu GameSummary_medium___r_ia')
-        for e in div:
-            if 'genre' in e.get_text().lower():
-                print(e.get_text())
+    if row[0] not in completed_rows:
+        page = requests.get("https://howlongtobeat.com/game/" + str(row[0]), headers=headers)
+        if page.status_code == 200:
+            soup = bs(page.content, "html.parser")
+            div = soup.find_all("div", class_='GameSummary_profile_info__HZFQu GameSummary_medium___r_ia')
+            for e in div:
+                if 'genre' in e.get_text().lower():
+                    txt = e.get_text().lower()
+                    genreTxt = txt[txt.find(':')+1].strip()
+                    cursor.execute('''INSERT INTO genres 
+                    (game_id, genre) VALUES (?, ?);
+                    ''', (row[0], genreTxt))
+                    conn.commit()
